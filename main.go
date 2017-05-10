2091d4df6e88463367000bcdc505ff47892d3ab1
Lucas Lima <lucas.lsl.lima@gmail.com>
2017-05-10 14:50:59 -0300

    Adding Task crud; Using Redis as primary data source

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"github.com/julienschmidt/httprouter"
 	"github.com/lucaslsl/goruncmds-api/app"
 	"github.com/namsral/flag"
 	"github.com/rs/cors"
+	"github.com/satori/go.uuid"
 	"log"
 	"net/http"
 	"time"
@@ -18,6 +20,7 @@ type TaskInstruction struct {
 }
 
 type Task struct {
+	ID           string            `json:"id"`
 	Name         string            `json:"name"`
 	Instructions []TaskInstruction `json:"instructions"`
 	ServersIDs   []string          `json:"servers_ids"`
@@ -49,18 +52,170 @@ func IsAuthenticated(w http.ResponseWriter, r *http.Request, _ httprouter.Params
 	return nil
 }
 
-func RunTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) error {
-	var t Task
-	err := app.ParseRequestBody(r, &t)
+func CreateTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) error {
+	var task Task
+	err := app.ParseRequestBody(r, &task)
 	if err != nil {
 		return err
 	}
-	tJson, _ := json.Marshal(t)
+	taskID := uuid.NewV4().String()
+	task.ID = taskID
+	taskJson, _ := json.Marshal(task)
 
 	redisConn := redisPool.Get()
 	defer redisConn.Close()
 
-	_, err = redisConn.Do("PUBLISH", *redisChannel, string(tJson))
+	redisKey := "task:" + taskID
+
+	_, err = redisConn.Do("SET", redisKey, string(taskJson))
+	if err != nil {
+		return err
+	}
+	_, err = redisConn.Do("SADD", "tasks", redisKey)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(task); err != nil {
+		return err
+	}
+	return nil
+}
+
+func RetrieveTask(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+
+	redisConn := redisPool.Get()
+	defer redisConn.Close()
+
+	task, err := redis.String(redisConn.Do("GET", "task:"+ps.ByName("taskID")))
+	if err != nil {
+		return app.ErrorResponse{http.StatusNotFound,
+			app.ErrNotFound, []app.ErrorResponseDetail{}}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, task)
+	return nil
+}
+
+func UpdateTask(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+
+	redisConn := redisPool.Get()
+	defer redisConn.Close()
+
+	redisKey := "task:" + ps.ByName("taskID")
+
+	t, err := redis.String(redisConn.Do("GET", redisKey))
+	if err != nil {
+		return app.ErrorResponse{http.StatusNotFound,
+			app.ErrNotFound, []app.ErrorResponseDetail{}}
+	}
+	var task Task
+	err = json.Unmarshal([]byte(t), &task)
+	if err != nil {
+		return err
+	}
+
+	var taskUpdated Task
+	err = app.ParseRequestBody(r, &taskUpdated)
+	if err != nil {
+		return err
+	}
+	taskUpdated.ID = task.ID
+
+	taskUpdatedJson, _ := json.Marshal(taskUpdated)
+	_, err = redisConn.Do("SET", redisKey, string(taskUpdatedJson))
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(taskUpdatedJson))
+	return nil
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+
+	redisConn := redisPool.Get()
+	defer redisConn.Close()
+
+	redisKey := "task:" + ps.ByName("taskID")
+
+	exists, err := redis.Bool(redisConn.Do("EXISTS", redisKey))
+	if err != nil || !exists {
+		return app.ErrorResponse{http.StatusNotFound,
+			app.ErrNotFound, []app.ErrorResponseDetail{}}
+	}
+
+	_, err = redisConn.Do("SREM", "tasks", redisKey)
+	if err != nil {
+		return err
+	}
+
+	_, err = redisConn.Do("DEL", redisKey)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+	return nil
+}
+
+func TaskList(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+
+	redisConn := redisPool.Get()
+	defer redisConn.Close()
+
+	members, err := redis.Strings(redisConn.Do("SMEMBERS", "tasks"))
+	if err != nil {
+		return err
+	}
+
+	var tasksToFetch []interface{}
+	for _, k := range members {
+		tasksToFetch = append(tasksToFetch, k)
+	}
+
+	values, err := redis.Strings(redisConn.Do("MGET", tasksToFetch...))
+	if err != nil {
+		return err
+	}
+
+	var tasks []Task
+	var tmp Task
+	for _, t := range values {
+		if json.Unmarshal([]byte(t), &tmp); err == nil {
+			tasks = append(tasks, tmp)
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(tasks); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func RunTask(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+	redisConn := redisPool.Get()
+	defer redisConn.Close()
+
+	redisKey := "task:" + ps.ByName("taskID")
+
+	task, err := redis.String(redisConn.Do("GET", redisKey))
+	if err != nil {
+		return app.ErrorResponse{http.StatusNotFound,
+			app.ErrNotFound, []app.ErrorResponseDetail{}}
+	}
+
+	_, err = redisConn.Do("PUBLISH", *redisChannel, task)
 	if err != nil {
 		return err
 	}
@@ -73,7 +228,12 @@ func RunTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) error 
 func main() {
 	redisPool = newPool(*redisAddr)
 	app := app.New()
-	app.PUT("/runtask", IsAuthenticated, RunTask)
+	app.POST("/tasks", IsAuthenticated, CreateTask)
+	app.GET("/tasks", IsAuthenticated, TaskList)
+	app.GET("/tasks/:taskID", IsAuthenticated, RetrieveTask)
+	app.PATCH("/tasks/:taskID", IsAuthenticated, UpdateTask)
+	app.DELETE("/tasks/:taskID", IsAuthenticated, DeleteTask)
+	app.PUT("/tasks/:taskID/run", IsAuthenticated, RunTask)
 	handler := cors.Default().Handler(app.Router)
 	log.Fatal(http.ListenAndServe(*listenAddr, handler))
 }
